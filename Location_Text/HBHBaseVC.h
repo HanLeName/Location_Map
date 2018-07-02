eee3b2b09712b7779d01849717c3de5e593cafa8
hanle <767081677@qq.com>
2018-07-02 14:39:30 +0800

    新建框架
    添加baseVC

---
+++ Location_Text/HBHBaseVC.h
@@ -0,0 +1,13 @@
+//
+//  HBHBaseVC.h
+//  Location_Text
+//
+//  Created by 易金科技 on 2018/7/2.
+//  Copyright © 2018年 韩保贺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HBHBaseVC : UIViewController
+
+@end
---
+++ Location_Text/HBHBaseVC.m
@@ -0,0 +1,37 @@
+//
+//  HBHBaseVC.m
+//  Location_Text
+//
+//  Created by 易金科技 on 2018/7/2.
+//  Copyright © 2018年 韩保贺. All rights reserved.
+//
+
+#import "HBHBaseVC.h"
+
+@interface HBHBaseVC ()
+
+@end
+
+@implementation HBHBaseVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
